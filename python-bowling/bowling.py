99fb8e30e030ae4f166956f7ffcd2b6381132473
sohaibtariq <sohaibtariq12@gmail.com>
2025-03-13 13:43:03 +0500

    init: uploaded python assignment

---
+++ python-bowling/bowling.py
@@ -0,0 +1,18 @@
+class BowlingGame:
+    def __init__(self):
+            # Initialize any variables you need to track the game state
+            pass
+
+    def roll(self, pins: int):
+        """
+        Called each time a player rolls a ball.
+        TODO: Implement this method to record the number of pins knocked down
+        """
+        pass
+
+    def score(self):
+        """
+        Called at the end of the game to calculate the total score.
+        TODO: Implement this method to calculate and return the total score
+        """
+        pass
